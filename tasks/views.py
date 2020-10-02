fe75be7a86ac077195d426af72e5c93443c5ce46
Silmar Pereira da Silva Junior <silmarjr2@gmail.com>
2020-10-02 17:36:45 -0300

    Added the ADD function to the code
        Now you can add a new Task object to the data base

--- tasks/views.py
+++ tasks/views.py
@@ -4,16 +4,27 @@ from .models import Task
 
 # Create your views here.
 def index(request):
-    tasks = ()
+    #### Query de objetos da classe Task e lista deles a ser passada para o front-end
+    tasks = []
     query = Task.objects.all()
-    print(query)
 
-    '''
-    A query ja funciona, falta só torna-la util
+
+    ### Adding a new task to the tasks
+    new_task = request.POST.get('new_task')
+    a = Task(text=new_task)
+    a.save()
+
+    ### Removing a task when button pressed
+
+
+    ###acrecentando o texto dos objetos TASK para uma lista a ser reendenizada no front-end
     for task in query:
-        task.append(task)
-    '''
+        tasks.append(task.text)
+    
 
+    ###Context, Dicionario de termos compartilhados entre o front e o back-end
+    context = { 'tasks': tasks,
+                'new_task': new_task
+    }
 
-
-    return render(request, 'base.html')
+    return render(request, 'base.html', context)
